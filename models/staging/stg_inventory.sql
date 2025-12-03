755f2896e531e148685b3ba5d4a0c15c984c86b7
Bhabnashre Dutta Roy <bhabnashre.work@gmail.com>
2025-12-03 07:05:13 +0000

    bdr commit

--- models/staging/stg_inventory.sql
+++ models/staging/stg_inventory.sql
@@ -1,1 +1,28 @@
-select * from {{ source ('src', 'raw_inventory')}}
+WITH source_data AS (
+    SELECT
+        INVENTORY_ID,
+        PRODUCT_ID,
+        WAREHOUSE_ID,
+        ON_HAND_QTY,
+        RESERVED_QTY,
+        ON_ORDER_QTY,
+        SNAPSHOT_DATE
+    FROM {{ source('src', 'raw_inventory') }}
+
+),
+
+cleaned AS (
+
+    SELECT
+        TRIM(INVENTORY_ID) AS inventory_id,
+        TRIM(PRODUCT_ID) AS product_id,
+        TRIM(WAREHOUSE_ID) AS warehouse_id,
+        COALESCE(ON_HAND_QTY, 0) AS on_hand_qty,
+        COALESCE(RESERVED_QTY, 0) AS reserved_qty,
+        COALESCE(ON_ORDER_QTY, 0) AS on_order_qty,
+        CAST(SNAPSHOT_DATE AS DATE) AS snapshot_date,
+        CURRENT_TIMESTAMP() AS record_loaded_ts
+    FROM source_data
+)
+
+SELECT * FROM cleaned
