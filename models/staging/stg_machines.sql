6267269b1a9aed4e0c0c5db20e05c41a918791a8
Vikyath Holla K <vikhyathholla111@gmail.com>
2025-12-03 06:59:23 +0000

    stg_machine_commit

--- models/staging/stg_machines.sql
+++ models/staging/stg_machines.sql
@@ -1,1 +1,8 @@
-select * from {{ source ('src', 'raw_machine')}}
+-- models/staging/stg_machine.sql
+SELECT
+    MACHINE_ID   AS machine_id,
+    MACHINE_TYPE AS machine_type,
+    DEPARTMENT   AS department,
+    INSTALL_DATE AS install_date,
+    STATUS       AS status
+FROM {{ source('src', 'raw_machine') }}
